3fc0970700aed64d18d86eb71cced663d9a7110c
Szymon Grabowski <szmiko@wp.pl>
2018-06-06 21:15:19 +0200

    Task

---
+++ redux-demo/src/actions.js
@@ -0,0 +1,34 @@
+import uuid from 'uuid';
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+	return {
+		type: ADD_COMMENT,
+		id: uuid.v4(),
+		text: text
+	}
+}
+
+function thumbUpComment(commentId) {
+	return {
+		type: THUMB_UP_COMMENT,
+		id: commentId
+	}
+}
+
+function thumbDownComment(commentId) {
+	return {
+		type: THUMB_DOWN_COMMENT,
+		id: commentId
+	}
+}
+
+export {THUMB_DOWN_COMMENT};
+export {thumbDownComment};
+export {THUMB_UP_COMMENT};
+export {thumbUpComment};
+export {ADD_COMMENT};
+export {addComment};
